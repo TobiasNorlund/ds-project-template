bb758f5cbbfed322a43a562d05f05ce20b4c0ee3
Tobias Norlund <tobias@norlund.se>
2020-11-23 10:58:32 +0100

    Added more start.sh options

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,43 +1,66 @@
 #!/bin/bash
+set -e
 
 # Docker image name for this project
-DOCKER_IMAGE_NAME="tobias/default"
+export DOCKER_IMAGE_NAME="tobias/default"
 
 # Path to where in the docker container the project root will be mounted
-DOCKER_WORKSPACE_PATH="/workspace"
+export DOCKER_WORKSPACE_PATH="/workspace"
 
 # Path to repo/project root dir (independent of pwd)
-PROJECT_ROOT=$( cd $(dirname $( dirname "${BASH_SOURCE[0]}") ); pwd )
+export PROJECT_ROOT=$( cd $(dirname $( dirname "${BASH_SOURCE[0]}") ); pwd )
 
 # Path to data dir
-DATA_DIR="$PROJECT_ROOT/data"
+export DATA_DIR="$PROJECT_ROOT/data"
 
-
-for arg in "$@"
+while [[ $# -gt 0 ]]
 do
-    if [ $arg == "gpu" ]; then
-      RUNTIME_ARGS="--gpus all"
-    elif [ $arg == "notebook" ]; then
-      JUPYTER_PORT="-p 8888:8888"
-    elif [ $arg == "tensorboard" ]; then
-      TENSORBOARD_PORT="-p 6006:6006"
-    else
-      echo "Unrecognized argument '$arg'"
-      exit 1
-    fi
+key="$1"
+
+case $key in
+    --gpu)
+    RUNTIME_ARGS="--gpus all"
+    shift # past argument
+    ;;
+    --notebook)
+    JUPYTER_PORT="-p 8888:8888"
+    shift # past argument
+    ;;
+    -v|--mount)
+    MOUNT="-v $2"
+    shift # past argument
+    shift # past value
+    ;;
+    --tensorboard)
+    TENSORBOARD_PORT="-p 6006:6006"
+    shift # past argument
+    ;;
+    -d|--detach)
+    DETACH="--detach"
+    shift # past argument
+    ;;
+    *)    # unknown option
+    echo "Unrecognized argument '$1'"
+    exit 1
+    ;;
+esac
 done
 
 USER_MAP="-u $(id -u):$(id -g)"
 CONTAINER_NAME=${DOCKER_IMAGE_NAME##*/}
 
+# Stop any potentially running container with the same name
+docker stop $CONTAINER_NAME 2> /dev/null || true
+
 set -x
 docker build --rm --build-arg DOCKER_WORKSPACE_PATH -t $DOCKER_IMAGE_NAME $PROJECT_ROOT
 docker run --rm -it \
   --name $CONTAINER_NAME \
-  -v /tmp:/tmp \
   -v $PROJECT_ROOT:$DOCKER_WORKSPACE_PATH \
+  $MOUNT \
   $USER_MAP \
   $RUNTIME_ARGS \
   $JUPYTER_PORT \
   $TENSORBOARD_PORT \
+  $DETACH \
   $DOCKER_IMAGE_NAME bash
